a7c5c73999b1a96fa0a8c93e6ed9e5c8466f168c
Winter <Winteralwin@gmail.com>
2022-05-14 21:14:21 +0200

    New Stuff

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import copy
 import tcod
 
 import entity_factories
-from input_handlers import EventHandler
 from engine import Engine
 from procgen import generate_dungeon, generate_dungeon_umberhulk
 
@@ -23,24 +22,23 @@ def main() -> None:
     max_monsters_per_room = 2
 
     tileset = tcod.tileset.load_tilesheet(
-        "E:\E\_Programming\Python Game\dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD # !! Update tileset (Also CHARMAP_TCOD is outdated?)
+        "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD # !! Update tileset (Also CHARMAP_TCOD is outdated?)
     )
-
-    event_handler = EventHandler()
 
     player = copy.deepcopy(entity_factories.player)
 
-    game_map = generate_dungeon( # !! Use more and different generators
+    engine = Engine(player=player)
+
+    engine.game_map = generate_dungeon( # !! Use more and different generators
         max_rooms=max_rooms,
         room_min_size=room_min_size,
         room_max_size=room_max_size,
         map_width=map_width,
         map_height=map_height,
         max_monsters_per_room=max_monsters_per_room,
-        player=player
+        engine=engine,
     )
-
-    engine = Engine(event_handler=event_handler, game_map=game_map, player=player)
+    engine.update_fov()
 
     with tcod.context.new_terminal(
         screen_width,
@@ -53,8 +51,7 @@ def main() -> None:
         while True:
             engine.render(console=root_console, context=context)
             
-            events = tcod.event.wait()
-            engine.handle_events(events)
+            engine.event_handler.handle_events()
 
 
 if __name__ == "__main__":
